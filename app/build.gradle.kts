5d9ea508a28101ff7ad1281071fd1394bdd987c6
pach24 <franciscopachecojob@gmail.com>
2026-02-13 12:24:14 +0100

    chore: bump version to 1.3.0 - Kotlin+Flow baseline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.nexosolar.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 3
+        versionName = "1.3.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
