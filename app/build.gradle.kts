ee28905612eb84aa7cfcc7da83e5ba654a6dcea2
pach24 <franciscopachecojob@gmail.com>
2026-03-02 12:18:47 +0100

    Fix: updated app version to 2.0.0 and version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nexosolar.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "2.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
